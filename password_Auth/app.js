77b58b8c7aa5093397cb9a398ef291843de15f6e
ibrahim oke <okeibrahin917@gmail.com>
2023-09-25 21:45:53 +0100

    first commit

---
+++ password_Auth/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
+const events = require("events");
+const event_emitter = new events.EventEmitter;
+
+let userIsAuth = false;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
+
+function passwordCheck(req,res,next){
+    let password = req.body["pass"]
+    if(password === "hibee"){
+        userIsAuth = true;
+    }
+    next();
+}
+app.use(passwordCheck);
+
+
+app.get("/", (req,res) =>{
+    res.render("submit")
+})
+
+
+app.post("/check",(req,res) => {
+if(userIsAuth){
+    res.render("secret");
+   }else{
+    res.render("submit");
+   }
+});
+
+app.post("/logout",(req,res) =>{
+    userIsAuth = false;
+    res.render("submit")
+})
+
+function Server(){
+    console.log(`Server running on ${port}`);
+}
+event_emitter.addListener("Start", Server);
+
+app.listen(port, () =>{
+    event_emitter.emit("Start");
+})
